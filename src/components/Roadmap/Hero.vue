0140ecf6f44e3a13d7782359f81c0e8613a2c6c1
ArtemSpiz <artemijspizhav@gmail.com>
2025-12-10 20:53:37 +0200

    Add Tokenomics, edits

--- src/components/Roadmap/Hero.vue
+++ src/components/Roadmap/Hero.vue
@@ -9,7 +9,7 @@ import Seats from "@/assets/img/Roadmap/SeatsHero.png";
     class="relative px-5 h-screen items-center pt-[120px] max-[400px]:bg-contain max-md:pt-[100px] text-center min-h-[500px] flex flex-col bg-[center_bottom] bg-cover bg-no-repeat bg-[url('@/assets/img/Roadmap/BgHero.png')] max-[400px]:bg-[url('@/assets/img/Roadmap/BgHeroMob.png')]"
   >
     <div
-      class="absolute bottom-[140px] max-[400px]:px-5 max-[400px]:bottom-[200px] right-1/2 translate-x-1/2 w-full max-w-[915px] h-full max-h-[400px] max-md:max-h-[500px] max-[400px]:max-h-[180px] max-md:bottom-0 z-[1] pointer-events-none overflow-hidden rounded-md"
+      class="absolute bottom-[140px] max-[400px]:px-5 max-[400px]:bottom-[200px] right-1/2 translate-x-1/2 w-full 2xl:max-w-[1600px] 2xl:max-h-[800px] max-w-[915px] h-full max-h-[400px] max-md:max-h-[500px] max-[400px]:max-h-[180px] max-md:bottom-0 z-[1] pointer-events-none overflow-hidden rounded-md"
     >
       <div
         class="absolute inset-0 z-[2] bg-gradient-to-t from-[#00000060] via-[#00000010] to-transparent pointer-events-none"
