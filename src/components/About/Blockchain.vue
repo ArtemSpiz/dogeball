342aa965f4366f7aae6f269b6fe9d6e300b8f8c0
ArtemSpiz <artemijspizhav@gmail.com>
2025-12-14 17:11:32 +0200

    edits

--- src/components/About/Blockchain.vue
+++ src/components/About/Blockchain.vue
@@ -20,7 +20,7 @@ const BlockchainCards = [
   },
   {
     number: "2",
-    text: `Click <span class="bg-[#EB4102] px-2 py-1 text-[9px] text-[#FFEEE1] rounded-[80px] inline-flex items-center align-middle">Connect Wallet</span> button at the top of the page to connect your wallet. Your wallet address will automatically populate in the faucet below`,
+    text: `Click <span class="bg-[#EB4102] px-2 py-1 text-[9px] text-[#FFEEE1] rounded-[80px]  items-center align-middle">Connect Wallet</span> button at the top of the page to connect your wallet. Your wallet address will automatically populate in the faucet below`,
   },
   {
     number: "3",
@@ -28,7 +28,7 @@ const BlockchainCards = [
   },
   {
     number: "4",
-    text: `Click the  <span class="bg-[#EB4102] px-2 py-1 text-[9px] text-[#FFEEE1] rounded-[80px] inline-flex items-center align-middle">Send</span> button below to claim your free $DOGEBALL test tokens from the faucet`,
+    text: `Click the  <span class="bg-[#EB4102] px-2 py-1 text-[9px] text-[#FFEEE1] rounded-[80px]  items-center align-middle">Send</span> button below to claim your free $DOGEBALL test tokens from the faucet`,
   },
   {
     number: "5",
