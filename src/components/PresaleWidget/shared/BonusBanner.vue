620a7bfb5fee19065d2cc0b00e7bdbb7ba3aa367
ArtemSpiz <artemijspizhav@gmail.com>
2025-12-18 17:25:54 +0200

    edits

--- src/components/PresaleWidget/shared/BonusBanner.vue
+++ src/components/PresaleWidget/shared/BonusBanner.vue
@@ -1,34 +1,15 @@
 <template>
   <div
-    class="flex flex-col justify-center items-center gap-3 rounded-2xl border border-white/20 bg-white/10 shadow-[0_0_14px_0_#5464D8] backdrop-blur-sm px-4 py-4 w-full max-w-[519px]"
+    class="flex flex-col justify-center items-center gap-3 rounded-2xl border border-white/20 bg-white/10 shadow-[0_0_14px_0_#5464D8] backdrop-blur-sm px-6 max-md:p-4 py-4 w-full max-w-[519px]"
   >
     <p
-      class="text-white text-center font-semibold text-lg leading-[90%] m-0 font-grotesk"
+      class="text-white text-center font-grotesk font-medium text-sm max-md:text-xs leading-[120%]"
     >
-      {{ t("presale.bonusBanner.presaleLaunchOffer") }}
+      {{ t("presale.bonusBanner.turnRewards") }}
+      <span class="font-semibold">{{
+        t("presale.bonusBanner.dashboardEarn")
+      }}</span>
     </p>
-
-    <div class="flex flex-wrap items-center justify-center gap-2 w-full">
-      <p
-        class="text-white text-center text-sm leading-tight m-0 whitespace-nowrap font-grotesk"
-      >
-        {{ t("presale.bonusBanner.useBonusCode") }}
-      </p>
-      <button
-        @click="copyBonusCode"
-        class="inline-flex font-grotesk items-center justify-center gap-1 px-2 font-black rounded-md bg-white text-[#080C23] text-sm leading-tight cursor-pointer transition-opacity hover:opacity-90 whitespace-nowrap font-grotesk"
-        :title="t('presale.bonusBanner.copyCode')"
-      >
-        {{ copied ? t("presale.bonusBanner.copied") : BONUS_CODE }}
-        <CopyIcon v-if="!copied" :size="16" color="#EB4102" />
-        <CheckIcon v-else :size="16" />
-      </button>
-      <p
-        class="text-white text-center text-sm leading-tight m-0 whitespace-nowrap font-grotesk"
-      >
-        {{ t("presale.bonusBanner.toGetMore") }}
-      </p>
-    </div>
   </div>
 </template>
 
