106113419f91f7ea98ba071894b327d95b5497d7
radoicic <illradoicic@gmail.com>
2026-01-03 00:56:25 +0800

    ticker

--- src/components/Home/Hero.vue
+++ src/components/Home/Hero.vue
@@ -3,10 +3,11 @@ import { useI18n } from "vue-i18n";
 import CustomButton from "@/ui/CustomButton.vue";
 import TimerCounter from "./TimerCounter.vue";
 import Circle from "@/assets/img/Home/CircleText.png";
+import { Vue3Marquee } from "vue3-marquee";
 
 const { t } = useI18n();
 import { useAccount, useToast } from "@/composables";
-import { ref } from "vue";
+import { ref, computed, onMounted, onUnmounted } from "vue";
 import Spinner from "../PresaleWidget/ui/Spinner.vue";
 import { presaleApi } from "@/api";
 
@@ -14,6 +15,23 @@ const accountData = useAccount();
 const loading = ref(false);
 const email = ref("");
 const { showSuccess, showInfo, showError } = useToast();
+
+const isMobile = ref(false);
+
+const updateIsMobile = () => {
+  isMobile.value = window.innerWidth <= 768;
+};
+
+onMounted(() => {
+  updateIsMobile();
+  window.addEventListener("resize", updateIsMobile);
+});
+
+onUnmounted(() => {
+  window.removeEventListener("resize", updateIsMobile);
+});
+
+const tickerDuration = computed(() => (isMobile.value ? 15 : 12));
 
 const submit = async () => {
   if (loading.value) return;
@@ -39,81 +57,93 @@ const submit = async () => {
 </script>
 
 <template>
-  <div
-    class="h-screen 2xl:h-[calc(100vh+100px)] 2xl:max-h-[1400px] 2xl:bg-bottom max-md:h-[calc(100vh-30px)] max-md:min-h-[660px] relative min-h-[600px] px-5 pt-[100px] flex flex-col items-center justify-between pb-[56px] bg-[url('@/assets/img/Home/HeroBg.png')] max-xl:!bg-cover bgFill bg-[center_bottom] bg-no-repeat"
-  >
-    <div class="text-center flex flex-col items-center justify-center w-full">
-      <div class="title max-w-[800px] px-3">
-        {{ t("hero.title") }}
-      </div>
-      <div class="title max-w-[800px] px-3 mt-2">
-        {{ t("hero.whitelistSignup") }}
-      </div>
-      <div
-        class="font-grotesk leading-[120%] text-base font-normal max-md:text-sm max-w-[470px] mt-4 max-md:mt-3 max-md:px-3"
-      >
-        {{ t("hero.subtitle") }}
-      </div>
-      <div
-        class="font-grotesk leading-[120%] text-base font-normal max-md:text-sm max-w-[470px] mt-6 max-md:mt-2"
-      >
-        {{ t("hero.presaleLaunch") }}
-      </div>
-      <TimerCounter />
-    </div>
-
+  <div class="relative">
     <div
-      class="max-w-[2000px] w-full flex justify-center items-center mx-auto relative max-md:static"
+      class="absolute top-[60px] left-0 right-0 bg-[#8B94F5] text-white py-1 overflow-hidden z-10 max-xl:top-[40px]"
     >
-      <div
-        class="flex absolute bottom-0 left-[120px] max-xl:left-[20px] max-lg:hidden w-full max-w-[325px] max-xl:max-w-[300px] flex-col p-4 gap-4 border border-[#8B94F5] rounded-2xl bg-[rgba(53,19,147,0.52)] backdrop-blur-[5px] shadow-[0_0_154px_0_#263166]"
+      <Vue3Marquee
+        :duration="tickerDuration"
+        :pause-on-hover="false"
+        :clone="true"
+        direction="normal"
       >
-        <div>
-          <div class="text-sm leading-[140%] font-grotesk">
-            {{ t("hero.whitelistEmail") }}
-          </div>
-          <input
-            class="py-1 w-full px-2 h-10 border border-[#DCDCDC] rounded-lg bg-[rgba(255,255,255,0.06)] text-base font-medium text-[rgba(255,255,255,0.30)]"
-            @input="(e) => (email = e.currentTarget.value)"
-            :placeholder="t('hero.emailPlaceholder')"
-          />
+        <div class="shrink-0 px-6 flex items-center">
+          <span class="font-grotesk font-semibold text-xs whitespace-nowrap">
+            $DOGEBALL TOKEN PRESALE IS LIVE!
+          </span>
         </div>
-        <CustomButton
-          :title="loading ? '' : t('hero.getEarlyAccess')"
-          class="w-full"
-          @click="submit"
+      </Vue3Marquee>
+    </div>
+    <div
+      class="h-screen 2xl:h-[calc(100vh+100px)] 2xl:max-h-[1400px] 2xl:bg-bottom max-md:h-[calc(100vh-30px)] max-md:min-h-[660px] relative min-h-[600px] px-5 pt-[100px] flex flex-col items-center justify-between pb-[56px] bg-[url('@/assets/img/Home/HeroBg.png')] max-xl:!bg-cover bgFill bg-[center_bottom] bg-no-repeat"
+    >
+      <div class="text-center flex flex-col items-center justify-center w-full">
+        <div class="title max-w-[800px] px-3">
+          {{ t("hero.title") }}
+        </div>
+        <div class="title max-w-[800px] px-3 mt-2">
+          {{ t("hero.whitelistSignup") }}
+        </div>
+        <div
+          class="font-grotesk leading-[120%] text-base font-normal max-md:text-sm max-w-[470px] mt-4 max-md:mt-3 max-md:px-3"
         >
-          <Spinner v-if="loading" :size="6" />
-        </CustomButton>
-      </div>
-
-      <div class="flex gap-4 items-center">
-        <a
-          target="_blank"
-          rel="noopener noreferrer"
-          href="/DOGEBALLCoinsultAudit.pdf"
+          {{ t("hero.subtitle") }}
+        </div>
+        <div
+          class="font-grotesk leading-[120%] text-base font-normal max-md:text-sm max-w-[470px] mt-6 max-md:mt-2"
         >
-          <CustomButton
-            :title="t('hero.audit')"
-            class="py-[18px] !text-[22px] !w-[160px] max-md:h-[50px]"
-        /></a>
-
-        <a
-          target="_blank"
-          rel="noopener noreferrer"
-          href="/DOGEBALLWhitepaper.pdf"
-        >
-          <CustomButton
-            :title="t('hero.whitepaper')"
-            white-bg="true"
-            class="py-[18px] !text-[22px] !w-[160px] max-md:h-[50px]"
-        /></a>
+          {{ t("hero.presaleLaunch") }}
+        </div>
+        <TimerCounter />
       </div>
 
       <div
-        class="w-[230px] max-md:w-[150px] absolute bottom-[-40px] max-lg:bottom-[50px] max-md:top-[400px] max-md:bottom-auto right-[120px] max-md:right-[10px] aspect-square"
+        class="max-w-[2000px] w-full flex justify-center items-center mx-auto relative max-md:static"
       >
-        <img :src="Circle" class="object-contain animate-rotateClockwise" />
+        <div
+          class="flex absolute bottom-0 left-[120px] max-xl:left-[20px] max-lg:hidden w-full max-w-[325px] max-xl:max-w-[300px] flex-col p-4 gap-4 border border-[#8B94F5] rounded-2xl bg-[rgba(53,19,147,0.52)] backdrop-blur-[5px] shadow-[0_0_154px_0_#263166]"
+        >
+          <div>
+            <div class="text-sm leading-[140%] font-grotesk">
+              {{ t("hero.whitelistEmail") }}
+            </div>
+            <input
+              class="py-1 w-full px-2 h-10 border border-[#DCDCDC] rounded-lg bg-[rgba(255,255,255,0.06)] text-base font-medium text-[rgba(255,255,255,0.30)]"
+              @input="(e) => (email = e.currentTarget.value)"
+              :placeholder="t('hero.emailPlaceholder')"
+            />
+          </div>
+          <CustomButton
+            :title="loading ? '' : t('hero.getEarlyAccess')"
+            class="w-full"
+            @click="submit"
+          >
+            <Spinner v-if="loading" :size="6" />
+          </CustomButton>
+        </div>
+
+        <div class="flex gap-4 items-center">
+          <a
+            target="_blank"
+            rel="noopener noreferrer"
+            href="/DOGEBALLCoinsultAudit.pdf"
+          >
+            <CustomButton
+              :title="t('hero.audit')"
+              class="py-[18px] !text-[22px] !w-[160px] max-md:h-[50px]"
+          /></a>
+
+          <a
+            target="_blank"
+            rel="noopener noreferrer"
+            href="/DOGEBALLWhitepaper.pdf"
+          >
+            <CustomButton
+              :title="t('hero.whitepaper')"
+              white-bg="true"
+              class="py-[18px] !text-[22px] !w-[160px] max-md:h-[50px]"
+          /></a>
+        </div>
       </div>
     </div>
   </div>
--- src/components/Home/ScrollLogos.vue
+++ src/components/Home/ScrollLogos.vue
@@ -1,4 +1,6 @@
 <script setup>
+import { Vue3Marquee } from "vue3-marquee";
+import { computed, onMounted, onUnmounted, ref } from "vue";
 import FeaturedLogo1 from "@/assets/img/Home/FeaturedLogo1.png";
 import FeaturedLogo2 from "@/assets/img/Home/FeaturedLogo2.png";
 import FeaturedLogo3 from "@/assets/img/Home/FeaturedLogo3.png";
@@ -16,11 +18,34 @@ const Logos = [
   { logo: FeaturedLogo6 },
   { logo: FeaturedLogo7 },
 ];
+
+const isMobile = ref(false);
+
+const updateIsMobile = () => {
+  isMobile.value = window.innerWidth <= 768;
+};
+
+onMounted(() => {
+  updateIsMobile();
+  window.addEventListener("resize", updateIsMobile);
+});
+
+onUnmounted(() => {
+  window.removeEventListener("resize", updateIsMobile);
+});
+
+const duration = computed(() => (isMobile.value ? 25 : 20));
 </script>
 
 <template>
-  <div class="overflow-hidden border-t p-6 max-md:p-5§ border-b border-white">
-    <div class="flex animate-scroll whitespace-nowrap">
+  <div class="border-t border-b border-white py-6 max-md:py-5">
+    <Vue3Marquee
+      :duration="duration"
+      :pause-on-hover="false"
+      :clone="true"
+      direction="normal"
+      class="marquee-container"
+    >
       <div
         v-for="(logo, idx) in Logos"
         :key="idx"
@@ -31,60 +56,13 @@ const Logos = [
           class="object-contain cursor-pointer h-full w-auto grayscale transition-all duration-300 hover:grayscale-0"
         />
       </div>
-
-      <div
-        v-for="(logo, idx) in Logos"
-        :key="idx"
-        class="h-7 w-auto shrink-0 mx-5"
-      >
-        <img
-          :src="logo.logo"
-          class="object-contain cursor-pointer h-full w-auto grayscale transition-all duration-300 hover:grayscale-0"
-        />
-      </div>
-
-      <div
-        v-for="(logo, idx) in Logos"
-        :key="idx"
-        class="h-7 w-auto shrink-0 mx-5"
-      >
-        <img
-          :src="logo.logo"
-          class="object-contain cursor-pointer h-full w-auto grayscale transition-all duration-300 hover:grayscale-0"
-        />
-      </div>
-
-      <div
-        v-for="(logo, idx) in Logos"
-        :key="idx"
-        class="h-7 w-auto shrink-0 mx-5"
-      >
-        <img
-          :src="logo.logo"
-          class="object-contain cursor-pointer h-full w-auto grayscale transition-all duration-300 hover:grayscale-0"
-        />
-      </div>
-    </div>
+    </Vue3Marquee>
   </div>
 </template>
 
 <style scoped>
-@keyframes scroll {
-  0% {
-    transform: translateX(0);
-  }
-  100% {
-    transform: translateX(-100%);
-  }
-}
-
-.animate-scroll {
-  animation: scroll 30s linear infinite;
-}
-
-@media (max-width: 768px) {
-  .animate-scroll {
-    animation: scroll 10s linear infinite;
-  }
+.marquee-container {
+  display: flex;
+  align-items: center;
 }
 </style>
