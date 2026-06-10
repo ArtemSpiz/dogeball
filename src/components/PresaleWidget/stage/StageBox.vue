0cfd6d28e39e0bafcf37162a6ffc49ffceaae32e
radoicic <150410633+radoicic@users.noreply.github.com>
2026-06-11 04:13:05 +0800

    Update StageBox.vue

--- src/components/PresaleWidget/stage/StageBox.vue
+++ src/components/PresaleWidget/stage/StageBox.vue
@@ -17,9 +17,9 @@
       </template>
       <p
         v-else
-        class="text-center relative text-white font-crisis text-2xl leading-none"
+        class="text-center relative text-white text-4xl font-extrabold"
         style="
-          font-weight: 400;
+          /* font-weight: 400; */
           -webkit-font-smoothing: antialiased;
           -moz-osx-font-smoothing: grayscale;
         "
@@ -27,18 +27,24 @@
         {{ formattedRaised }}
       </p>
 
-      <Countdown v-if="stageEndDate" :end-date="stageEndDate" @on-end="refetchStage" />
-      <span class="text-white font-grotesk text-xs font-normal leading-none my-1">
+      <Countdown
+        v-if="stageEndDate"
+        :end-date="stageEndDate"
+        @on-end="refetchStage"
+      />
+      <span
+        class="text-white font-grotesk text-xs font-normal leading-none my-1"
+      >
         {{ t("presale.stageBox.untilPriceRise") }}
       </span>
       <!-- Progress Bar -->
       <ProgressBar :progress="stageFrac * 100" variant="striped" size="md" />
-        <!-- Participants -->
-        <p
-          class="text-white text-center w-full font-grotesk text-md font-normal leading-none flex-1"
-        >
-          {{ formattedParticipants }} {{ t("presale.stageBox.participants") }}
-        </p>
+      <!-- Participants -->
+      <p
+        class="text-white text-center w-full font-grotesk text-md font-normal leading-none flex-1"
+      >
+        {{ formattedParticipants }} {{ t("presale.stageBox.participants") }}
+      </p>
     </div>
   </div>
 </template>
@@ -62,37 +68,45 @@ const stageName = computed(() => {
   return presale.stage.value?.stage_name || DEFAULT_STAGE_NAME;
 });
 
-const stageEndDate = computed(() => presale.stage.value?.stage_end ? new Date(presale.stage.value.stage_end) : undefined)
+const stageEndDate = computed(() =>
+  presale.stage.value?.stage_end
+    ? new Date(presale.stage.value.stage_end)
+    : undefined,
+);
 
 const minMax = (num, min, max) => {
-  if (num < min) return min
-  if (num > max) return max
-  return num
-}
+  if (num < min) return min;
+  if (num > max) return max;
+  return num;
+};
 
 const stageFrac = computed(() => {
-  if (presale.presaleEnded.value) return 1
-  const stage = presale.stage.value
-  if (!stage) return 0
+  if (presale.presaleEnded.value) return 1;
+  const stage = presale.stage.value;
+  if (!stage) return 0;
   const tokenProgress = minMax(
-    parseNum(stage.cumulative_tokens_sold) / parseNum(stage.next_stage_target_tokens),
+    parseNum(stage.cumulative_tokens_sold) /
+      parseNum(stage.next_stage_target_tokens),
     0,
-    1
-  )
+    1,
+  );
   if (!stage.stage_end) return tokenProgress;
-  const stageStartTimestamp = new Date(stage.stage_start ?? Date.now()).getTime()
-  const stageEndTimestamp = new Date(stage.stage_end ?? Date.now()).getTime()
+  const stageStartTimestamp = new Date(
+    stage.stage_start ?? Date.now(),
+  ).getTime();
+  const stageEndTimestamp = new Date(stage.stage_end ?? Date.now()).getTime();
   const countdownProgress = minMax(
-    (Date.now() - stageStartTimestamp) / (stageEndTimestamp - stageStartTimestamp),
+    (Date.now() - stageStartTimestamp) /
+      (stageEndTimestamp - stageStartTimestamp),
     0,
-    1
-  )
-  return Math.max(countdownProgress, tokenProgress)
+    1,
+  );
+  return Math.max(countdownProgress, tokenProgress);
 });
 
 const progressPercent = computed(() => {
   return Intl.NumberFormat("en-US", { maximumFractionDigits: 0 }).format(
-    (1 - stageFrac.value) * 100
+    (1 - stageFrac.value) * 100,
   );
 });
 
@@ -101,7 +115,7 @@ const formattedRaised = computed(() => {
     parseNum(presale.stage.value?.cumulative_usd_raised),
     false,
     0,
-    2
+    2,
   );
 });
 
@@ -110,7 +124,7 @@ const formattedTarget = computed(() => {
     parseNum(presale.stage.value?.next_stage_target_usd),
     false,
     0,
-    2
+    2,
   );
 });
 
